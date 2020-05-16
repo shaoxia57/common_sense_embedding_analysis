0da9f049ac4d3bdd1fc03a7db13f3453f477dbb2
shaoxia57 <peiz@usc.edu>
2020-05-16 15:46:02 -0700

    roberta-large ft

--- experiment_utils/run_mwp_roberta_finetuned_hf_eval.py
+++ experiment_utils/run_mwp_roberta_finetuned_hf_eval.py
@@ -55,12 +55,13 @@ def main():
     # state_dict = torch.load(checkpoint_path)["model"]
     # roberta.load_state_dict(state_dict)
 
-    roberta = HappyROBERTA('roberta-base')
+    roberta = HappyROBERTA('roberta-large')
     
-    config = RobertaConfig.from_pretrained('roberta-base')
+    config = RobertaConfig.from_pretrained('roberta-large')
     mlm = RobertaForMaskedLM(config)
     #checkpoint_path = '/home/rahul/common_sense_embedding_analysis/data/finetune_data/save_step_92160/checkpoint.pt'
-    checkpoint_path = '/home/rahul/common_sense_embedding_analysis/data/finetune_data/roberta-base/save_step_230400/checkpoint.pt'
+    #checkpoint_path = '/home/rahul/common_sense_embedding_analysis/data/finetune_data/roberta-base/save_step_230400/checkpoint.pt'
+    checkpoint_path = '/home/rahul/common_sense_embedding_analysis/data/finetune_data/roberta-large/old_roberta_large_no_change/checkpoint.pt'
     state_dict = torch.load(checkpoint_path)["model"]
     mlm.load_state_dict(state_dict)
     mlm.eval()
@@ -92,7 +93,7 @@ def main():
                              number_of_entity_trials=number_of_entity_trials,
                              logger=logger)
 
-    output_df.to_csv("../data/masked_word_result_data/roberta-base/physical_perf_ft20_new_{}.csv".format(number_of_entity_trials),
+    output_df.to_csv("../data/masked_word_result_data/roberta/physical_perf_ft_new_{}.csv".format(number_of_entity_trials),
                      index=False)
 
     logger.info("finished saving physical dataset results")
@@ -117,7 +118,7 @@ def main():
                              number_of_entity_trials=number_of_entity_trials,
                              logger=logger)
 
-    output_df.to_csv("../data/masked_word_result_data/roberta-base/material_perf_ft20_new_{}.csv".format(number_of_entity_trials),
+    output_df.to_csv("../data/masked_word_result_data/roberta/material_perf_ft_new_{}.csv".format(number_of_entity_trials),
                      index=False)
 
     logger.info("finished saving physical material results")
@@ -141,7 +142,7 @@ def main():
                              number_of_entity_trials=number_of_entity_trials,
                              logger=logger)
 
-    output_df.to_csv("../data/masked_word_result_data/roberta-base/social_perf_ft20_new_{}.csv".format(number_of_entity_trials),
+    output_df.to_csv("../data/masked_word_result_data/roberta/social_perf_ft_new_{}.csv".format(number_of_entity_trials),
                      index=False)
 
     logger.info("finished saving physical social results")
